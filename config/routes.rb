77b3eca40f1a5c8e88631c1b4017dea3215ccf85
Lori Burleson <lori.burleson@gmail.com>
2016-11-14 18:26:04 -0500

    added route and method for toggling boolean active field

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'api/v1/sitters' => 'sitters#index'
 
+  patch 'api/v1/family/:id' => 'families#toggle_active_family'
+
   get 'agency/index' => 'agency#index'
   get 'agency/families' => 'agency#families'
   get 'agency/new' => 'agency#new'
