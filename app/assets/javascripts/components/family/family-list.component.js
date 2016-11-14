71da387bc02c003bd9e2c60c829f6e161e9e0576
Marliana Lara <marliana.lara@gmail.com>
2016-11-14 08:47:04 -0500

    add change status function

--- app/assets/javascripts/components/family/family-list.component.js
+++ app/assets/javascripts/components/family/family-list.component.js
@@ -12,10 +12,23 @@
 
     function FamilyListController(familyAPI) {
       var ctrl = this;
+
       ctrl.$onInit = function() {
         familyAPI.list().then(function(response) {
           ctrl.families = response.data;
+        }, function(response) {
+          alert('Failed');
         });
+      };
+
+      ctrl.changeStatus = function() {
+        console.log('change');
+        if (this.active) {
+          this.active = false;
+          console.log(this.active);
+        } else {
+          this.active = true;
+        }
       };
 
     }
