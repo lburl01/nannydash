2c0c1de0d1d7e0d5773cd516e3e2c9503505bd23
Lori Burleson <lori.burleson@gmail.com>
2016-11-15 08:28:52 -0500

    refactored sitter controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,32 @@ class User < ApplicationRecord
   validates :phone_number, presence: true, format: { with: VALID_PHONE_REGEX }
   validate :picture_size
 
+  def self.get_active_sitters
+    data = User.nanny.all
+
+    @sitters = []
+
+    data.each do |sitter|
+      address = "#{sitter.street}, #{sitter.city}, #{sitter.state}, #{sitter.zip_code}"
+      recommendation_one = "#{sitter.recommendation_one_name}, #{sitter.recommendation_one_email}"
+      recommendation_two = "#{sitter.recommendation_two_name}, #{sitter.recommendation_two_email}"
+      recommendation_three = "#{sitter.recommendation_three_name}, #{sitter.recommendation_three_email}"
+      @sitters << {"sitter_id" => sitter.id, "first_name" => sitter.first_name,
+                      "last_name" => sitter.last_name, "email" => sitter.email,
+                      "phone" => sitter.phone_number, "county" => sitter.county,
+                      "birthday" => sitter.birthday, "address" => address,
+                      "hourly_rate" => sitter.hourly_rate,
+                      "cpr_cert" => sitter.cpr_certification,
+                      "first_aid_cert" => sitter.first_aid_certification,
+                      "recomendation_one" => recommendation_one,
+                      "recomendation_two" => recommendation_two,
+                      "recomendation_three" => recommendation_three,
+                      "joined" => sitter.created_at
+                    }
+    end
+    return @sitters
+  end
+
   private
 
     def picture_size
