8c2838c74a9b6be0e1fd6a926a6424ede3830b3c
Lori Burleson <lori.burleson@gmail.com>
2016-11-15 09:24:06 -0500

    removed basepath of localhost

--- app/assets/javascripts/components/family/familyAPI.service.js
+++ app/assets/javascripts/components/family/familyAPI.service.js
@@ -1,18 +1,17 @@
 (function() {
   'use strict';
   angular.module('app').factory('familyAPI', ['$http', function($http) {
-    var basePath = "http://localhost:3000";
 
     return {
       list: function() {
-        return $http.get(basePath + "/agency/families.json").then(function(response) {
+        return $http.get("/agency/families.json").then(function(response) {
           return response.data;
         }, function() {
           alert('Failed');
         });
       },
       toggleActiveState: function(id) {
-        return $http.patch(basePath + "/api/v1/family/" + id);
+        return $http.patch("/api/v1/family/" + id);
       }
     };
 
