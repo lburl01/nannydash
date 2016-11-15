494c12c05be366a88a29759ac51f5c5169224045
Lori Burleson <lori.burleson@gmail.com>
2016-11-15 11:05:20 -0500

    passing patch toggle family test

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   VALID_ZIP_REGEX = /\d{5}/
   VALID_BIRTHDAY_REGEX = /\d{2}[\/]\d{2}[\/]\d{4}/
   VALID_RATE_REGEX = /\d+[.]\d{2}/
-  # VALID_PHONE_REGEX = /\(*\+*[1-9]{0,3}\)*-*[1-9]{0,3}[-. \/]*\(*[2-9]\d{2}\)*[-. \/]*\d{3}[-. \/]*\d{4} *e*x*t*\.* *\d{0,4}/
+  VALID_PHONE_REGEX = /\(*\+*[1-9]{0,3}\)*-*[1-9]{0,3}[-. \/]*\(*[2-9]\d{2}\)*[-. \/]*\d{3}[-. \/]*\d{4} *e*x*t*\.* *\d{0,4}/
 
   validates_presence_of :first_name, :last_name, :email, :street, :city, :county
   validates :state, presence: true, length: { maximum: 2},
@@ -22,7 +22,7 @@ class User < ApplicationRecord
   validates :zip_code, presence: true, format: { with: VALID_ZIP_REGEX }
   validates :birthday, allow_nil: true, format: { with: VALID_BIRTHDAY_REGEX }
   validates :hourly_rate, allow_nil: true, format: { with: VALID_RATE_REGEX }
-  # validates :phone_number, presence: true, format: { with: VALID_PHONE_REGEX }
+  validates :phone_number, presence: true, format: { with: VALID_PHONE_REGEX }
   validate :picture_size
 
   def self.get_active_sitters
--- spec/controllers/families_controller_spec.rb
+++ spec/controllers/families_controller_spec.rb
@@ -3,12 +3,16 @@ require 'rails_helper'
 RSpec.describe FamiliesController, type: :controller do
 
   describe "PATCH #toggle_active_family" do
-    it "toggles a family active boolean" do
+    it('works!') do
       @user = create(:user)
 
-      toggle_active_family
+      expect(@user.active).to eq false
 
+      patch :toggle_active_family, id: @user.id
+      @user.reload
+      
       expect(@user.active).to eq true
+
     end
   end
 
