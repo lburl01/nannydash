73be5497c017e9099575beecc7154eb75572d54b
Lori Burleson <lori.burleson@gmail.com>
2016-11-16 16:10:58 -0500

    Passing refactored messageable test

--- spec/models/messageable_spec.rb
+++ spec/models/messageable_spec.rb
@@ -5,17 +5,24 @@ RSpec.describe Messageable do
     @user.extend(Messageable)
   end
 
-  it "will create a new conversation and message" do
+  it "will create a new conversation" do
     @sender = build(:user)
     @recipient = build(:recipient)
     subject = "Subject Line"
     body = "Lots of text goes here"
 
+    expect{@sender.send_message(@recipient, body, subject)}.to \
+      change{ Conversation.count }.from(0).to(1)
+  end
+
+  it "will create a new message" do
+    @sender = build(:user, id: 3)
+    @recipient = build(:recipient, id: 4)
+    subject = "Subject Line"
+    body = "Lots of text goes here"
 
     expect{@sender.send_message(@recipient, body, subject)}.to \
-      change{ [Conversation.count, Message.count] }.from([0,0]).to([1,1])
-    # expect{@sender.send_message(@recipient, body, subject)}.to \
-      # change{Message.count}.from(0).to(1)
+      change{ Message.count }.from(0).to(1)
   end
 
 end
