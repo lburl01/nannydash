7e6d8f4fc54c29ab20511605c32c8e21d5e4076b
Lori Burleson <lori.burleson@gmail.com>
2016-11-17 13:02:05 -0500

    Can get both names of family and sitter via user table

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,6 @@
 class Job < ApplicationRecord
-  # belongs_to :user
-  belongs_to :posted_job, :class_name => "Job", foreign_key: "family_id"
-  belongs_to :assignment, :class_name => "Job", foreign_key: "sitter_id"
+  belongs_to :posted_job, :foreign_key => :family_id, class_name: 'User'
+  belongs_to :assignment, :foreign_key => :sitter_id, class_name: 'User'
 
   def self.get_assigned_jobs
     response = Job.where(is_deleted: false).where(is_assigned: true).all
@@ -9,9 +8,9 @@ class Job < ApplicationRecord
     @jobs = []
 
     response.each do |job|
-      debugger
       @jobs << { "job_id" => job.id,
-                 "family_first_name" => job.posted_job.first_name
+                 "family_first_name" => job.posted_job.first_name,
+                 "sitter_first_name" => job.assignment.first_name
                }
     end
 
