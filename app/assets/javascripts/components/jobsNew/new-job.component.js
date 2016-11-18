27d65d329845e29994d09c16c6eb6aca392dece4
Marliana Lara <marliana.lara@gmail.com>
2016-11-17 21:07:34 -0500

    style new jobs table

---
+++ app/assets/javascripts/components/jobsNew/new-job.component.js
@@ -0,0 +1,18 @@
+(function() {
+  'use strict';
+  angular
+    .module('app')
+    .component('newJob', {
+      bindings: {
+        job: '<'
+      },
+      templateUrl: 'new-job.html',
+      controller: ['newJobsAPI', NewJobController]
+    });
+
+  function NewJobController(newJobsAPI) {
+
+
+  }
+
+})();
