d6e9706877ba6474ff13d4b469fbf363e11c69a6
Chloe Collier <achloecollier@gmail.com>
2016-11-18 09:07:03 -0500

    added data endpoints to dashboard

--- app/assets/javascripts/components/dashboard/dashboard-controller.js
+++ app/assets/javascripts/components/dashboard/dashboard-controller.js
@@ -1,8 +1,21 @@
 angular.module('app')
-    .controller('dashboardController', ["$scope", "$http", "$state", "$stateParams", function ($scope, $http, $state, $stateParams) {
+    .controller('dashboardController', ["dashboardAPI", "$http", "$state", "$stateParams", function (dashboardAPI, $http, $state, $stateParams) {
       /*************************
       Variables
       *************************/
+      var self = this;
+
+      dashboardAPI.list().success(function(response) {
+        console.log(response);
+        self.application = response.applications
+        self.assignments = response.assignments
+        self.messages = response.messages;
+        self.openJobs = response.open_jobs;
+
+
+      }, function(response) {
+        alert('Failed');
+      });
 
 
   }]);
--- app/assets/javascripts/components/dashboard/dashboard-service.js
+++ app/assets/javascripts/components/dashboard/dashboard-service.js
@@ -3,7 +3,7 @@ angular.module('app').service('dashboardAPI', ['$http', function($http) {
     return {
       list: function() {
         return $http({
-          url: "api/v1/agency/summary",
+          url: "/api/v1/agency/summary",
           method: "GET"
         });
       }
