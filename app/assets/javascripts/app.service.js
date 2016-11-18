1bc4400175571f085e4dfe6232637c394f64acb8
Marliana Lara <marliana.lara@gmail.com>
2016-11-18 15:37:11 -0500

    add count endpoint

--- app/assets/javascripts/app.service.js
+++ app/assets/javascripts/app.service.js
@@ -1,6 +1,16 @@
 (function() {
   'use strict';
-  app.module('app').factory('appAPI', {
-    
-  });
+  angular.module('app').factory('appAPI', ['$http', function($http){
+    return {
+
+      totalCount: function() {
+        return $http.get("api/v1/agency/count_totals").then(function(response) {
+          return response.data;
+        }, function() {
+          alert('Failed');
+        });
+      }
+
+    };
+  }]);
 })();
