b950f54fc422b9e907938e985e740d2096abb348
SamDC <sam.davis.castro@gmail.com>
2016-11-18 15:01:35 -0500

    added ability to update number of total jobs based on the number of elements in the array when the page loads.

--- app/assets/javascripts/components/jobsAll/allJobsController.js
+++ app/assets/javascripts/components/jobsAll/allJobsController.js
@@ -6,10 +6,24 @@
     'use strict';
     angular.module('app')
         .controller('allJobsController', ['allJobsAPI', function(allJobsAPI) {
-allJobsAPI.jobsList().success(function(response){
-  console.log(response);
-});
-
+            var self = this;
+            allJobsAPI.jobsList().success(function(response) {
+                self.totalJobs = response;
+                console.log(response);
+            });
+            this.name = function(firstName, lastName) {
+                return firstName + " " + lastName;
+            };
+            this.time = function(startTime, endTime) {
+                return startTime + " - " + endTime;
+            };
+            this.confirmed = function(confirmation) {
+                if (confirmation === true) {
+                    return "Yes";
+                } else {
+                    return "No";
+                }
+            };
         }]);
 
 })();
