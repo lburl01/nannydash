25e6cc9dc3e3568874ce6202778802d11d9db3bf
Lori Burleson <lori.burleson@gmail.com>
2016-11-21 10:41:29 -0500

    prepped files for jobs controller testing

---
+++ spec/controllers/jobs_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe JobsController, type: :controller do
+
+  describe 'GET #index' do 
+    it 'returns array of jobs with sitter assigned' do
+
+    end
+  end
+
+end
