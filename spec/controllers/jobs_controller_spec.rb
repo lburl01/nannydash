dafb9d10b2286681393e9e7135f7ede042e494e3
Lori Burleson <lori.burleson@gmail.com>
2016-11-21 10:52:36 -0500

    passing get #index

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -2,9 +2,18 @@ require 'rails_helper'
 
 RSpec.describe JobsController, type: :controller do
 
-  describe 'GET #index' do 
+  describe 'GET #index' do
     it 'returns array of jobs with sitter assigned' do
+      @sitter = create(:user)
+      @family = create(:recipient)
+      @first_job = create(:job)
+      @assigned_job = create(:assigned_job)
 
+      get :index
+
+      json = JSON.parse(response.body)
+
+      expect(json.length).to eq 1
     end
   end
 
