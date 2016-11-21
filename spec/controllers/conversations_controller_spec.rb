44b72193c2a02fe385bb7e9b764ca98989b36def
Lori Burleson <lori.burleson@gmail.com>
2016-11-21 09:47:01 -0500

    working on conversation test

--- spec/controllers/conversations_controller_spec.rb
+++ spec/controllers/conversations_controller_spec.rb
@@ -1,19 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ConversationsController, type: :controller do
-  config.include Devise::Test::ControllerHelpers
 
-  before do
-    @user = create(:user)
-    login_as @user
-  end
+    
 
-  context 'when logged in as agency manager' do
-    describe "GET #index" do
-      it "returns http success" do
-        get :index
-        expect(response).to have_http_status(:success)
-      end
-    end
-  end
 end
