69396b061e1b4afb3b6928a120c2635f36fc4d8d
Lori Burleson <lori.burleson@gmail.com>
2016-11-21 10:56:18 -0500

    passing get #get_new_jobs test

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -2,14 +2,28 @@ require 'rails_helper'
 
 RSpec.describe JobsController, type: :controller do
 
+  before do
+    @sitter = create(:user)
+    @family = create(:recipient)
+    @first_job = create(:job)
+    @assigned_job = create(:assigned_job)
+  end
+
   describe 'GET #index' do
     it 'returns array of jobs with sitter assigned' do
-      @sitter = create(:user)
-      @family = create(:recipient)
-      @first_job = create(:job)
-      @assigned_job = create(:assigned_job)
 
       get :index
+
+      json = JSON.parse(response.body)
+
+      expect(json.length).to eq 1
+    end
+  end
+
+  describe 'GET #get_new_jobs' do
+    it 'returns an array of jobs without a sitter' do
+
+      get :get_new_jobs
 
       json = JSON.parse(response.body)
 
