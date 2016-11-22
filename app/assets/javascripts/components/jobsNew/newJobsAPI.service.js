daa4d7673c0742a772530dff91b2bed7c7a285c6
Marliana Lara <marliana.lara@gmail.com>
2016-11-21 19:05:02 -0500

    change input background and cursor of pending parent form

--- app/assets/javascripts/components/jobsNew/newJobsAPI.service.js
+++ app/assets/javascripts/components/jobsNew/newJobsAPI.service.js
@@ -13,7 +13,6 @@
 
       jobInfo: function(id) {
         return $http.get("/api/v1/job/" + id + ".json").then(function(response) {
-          console.log(response.data);
           return response.data;
         }, function() {
           alert('Failed');
