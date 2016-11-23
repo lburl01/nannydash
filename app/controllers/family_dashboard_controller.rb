416c1275b91c8d78a463f35d64107d1aea447d37
Lori Burleson <lori.burleson@gmail.com>
2016-11-23 14:32:33 -0500

    Fixed weird redirect after log in issue

--- app/controllers/family_dashboard_controller.rb
+++ app/controllers/family_dashboard_controller.rb
@@ -1,5 +1,9 @@
 class FamilyDashboardController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :authenticate_user!
+
   def index
     render :index, layout: false
   end
+
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,5 @@ class HomeController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    @users = User.manager.all
   end
 end
