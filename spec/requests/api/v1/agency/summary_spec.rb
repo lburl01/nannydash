a05ce6089a54eb438c9d3b21ee988baf336b8448
Lori Burleson <lori.burleson@gmail.com>
2016-11-26 16:35:34 -0500

    Passing logged in user and agency show request specs

--- spec/requests/api/v1/agency/summary_spec.rb
+++ spec/requests/api/v1/agency/summary_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "GET /api/v1/agency/summary" do
+  before do
+    sign_in create(:user)
+  end
 
   it "returns counts of messages, pending sitter and " +
   "family applications, assignments and open jobs" do
-    current_user = create(:user)
 
-    sign_in(current_user)
     get '/api/v1/agency/summary'
 
     expect(json_body.keys).to contain_exactly("applications", "assignments", "messages", "open_jobs")
   end
+
 end
