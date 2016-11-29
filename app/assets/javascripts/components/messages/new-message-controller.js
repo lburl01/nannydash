d91d539078118f03bf727737ccc2f745d5217ea4
Chloe Collier <achloecollier@gmail.com>
2016-11-29 14:42:54 -0500

    made changes to new messages

--- app/assets/javascripts/components/messages/new-message-controller.js
+++ app/assets/javascripts/components/messages/new-message-controller.js
@@ -9,15 +9,19 @@ angular.module('app')
       dashboardAPI.activeBabysitters().success(function(response) {
         var nannysArray = [];
         var parentsArray = [];
+        var managerArray = [];
         for(var i = 0; i < response.length; i++) {
           if(response[i].role === 'nanny') {
             nannysArray.push(response[i]);
           } else if(response[i].role === 'family') {
             parentsArray.push(response[i]);
+          } else if(response[i].role === 'manager') {
+            managerArray.push(response[i]);
           }
         };
         self.nannys = nannysArray;
         self.parents = parentsArray;
+        self.manager = managerArray;
 
       });
 
