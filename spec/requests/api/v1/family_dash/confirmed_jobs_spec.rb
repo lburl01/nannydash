6fd21a74227e0d17c190cd45cead0d1294a473c9
Lori Burleson <lori.burleson@gmail.com>
2016-11-28 22:24:02 -0500

    passing family dash confirmed jobs request spec

--- spec/requests/api/v1/family_dash/confirmed_jobs_spec.rb
+++ spec/requests/api/v1/family_dash/confirmed_jobs_spec.rb
@@ -1,44 +1,47 @@
 require 'rails_helper'
 
-RSpec.describe "GET sitter_dash/confirmed_jobs" do
+RSpec.describe "GET family_dash/confirmed_jobs" do
   before do
     create(:user)
-    @sitter = create(:recipient)
-    sign_in @sitter
-    create(:family)
+    @family = create(:family)
+    sign_in @family
+    create(:recipient)
     create(:job)
-    create(:assigned_job, sitter_id: @sitter.id, confirmed: true, is_assigned: true)
+    create(:assigned_job, family_id: @family.id, confirmed: true, is_assigned: true)
   end
 
   it "returns array with one item" do
 
-    get "/sitter_dash/confirmed_jobs"
+    get "/family_dash/confirmed_jobs"
 
     expect(json_body.length).to eq 1
   end
 
   it "contains exactly the keys confirmed, date, end_time, family_id, " +
-    "family_name, is_assigned, job_id, notes, sitter_id, " +
-    "start_time" do
+    "is_assigned, job_id, notes, sitter_id, sitter_name, start_time" do
 
-    get "/sitter_dash/confirmed_jobs"
+    get "/family_dash/confirmed_jobs"
 
     expect(json_body[0].keys).to contain_exactly("confirmed", "date", "end_time",
-      "family_id", "family_name", "is_assigned", "job_id", "notes", "sitter_id",
+      "family_id", "is_assigned", "job_id", "notes", "sitter_id", "sitter_name",
       "start_time")
   end
 
   it 'only returns is_assigned: true jobs' do
 
-    get "/sitter_dash/confirmed_jobs"
+    get "/family_dash/confirmed_jobs"
 
     expect(json_body[0]["is_assigned"]).to eq true
   end
 
   it 'only returns confirmed: true jobs' do
 
-    get "/sitter_dash/confirmed_jobs"
+    get "/family_dash/confirmed_jobs"
 
     expect(json_body[0]["confirmed"]).to eq true
+  end
+
+  it 'only returns jobs for logged in family' do
+
   end
 end
