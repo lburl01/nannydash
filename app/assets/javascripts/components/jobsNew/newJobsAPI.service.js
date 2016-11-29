2738765ced16b2d54b938410cc6dd70cd7276624
Marliana Lara <marliana.lara@gmail.com>
2016-11-29 18:35:17 -0500

    add ability to assign sitter on new jobs page

--- app/assets/javascripts/components/jobsNew/newJobsAPI.service.js
+++ app/assets/javascripts/components/jobsNew/newJobsAPI.service.js
@@ -22,6 +22,22 @@
 
       deleteJob: function(id) {
         return $http.patch("/api/v1/job/delete/" + id + ".json", {id: id});
+      },
+
+      activeUsers: function() {
+        return $http.get('/users/all_active.json').then(function(response) {
+          return response.data;
+        }, function() {
+          console.log('Failed to get active users');
+        });
+      },
+
+      assignSitter: function(data) {
+        return $http.patch('/api/v1/job/agency_assign/' + data.id + '.json', {id: data.jobId, sitter_id: data.sitterId}).then(function(response) {
+          console.log('sitter was assigned successfully');
+        }, function() {
+          console.log('Sitter was not assigned.');
+        });
       }
 
     };
