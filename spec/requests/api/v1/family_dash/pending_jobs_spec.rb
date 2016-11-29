484e190f9e1fae690d1c7116c1105c81057fe3a8
Lori Burleson <lori.burleson@gmail.com>
2016-11-28 22:27:25 -0500

    passing family dash pending jobs request spec

--- spec/requests/api/v1/family_dash/pending_jobs_spec.rb
+++ spec/requests/api/v1/family_dash/pending_jobs_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "GET family_dash/confirmed_jobs" do
+RSpec.describe "GET family_dash/pending_jobs" do
   before do
     create(:user)
     @family = create(:family)
     sign_in @family
     create(:recipient)
     create(:job)
-    create(:assigned_job, family_id: @family.id, confirmed: true, is_assigned: true)
+    create(:assigned_job, family_id: @family.id, confirmed: false)
   end
 
   it "returns array with one item" do
 
-    get "/family_dash/confirmed_jobs"
+    get "/family_dash/pending_jobs"
 
     expect(json_body.length).to eq 1
   end
@@ -20,29 +20,22 @@ RSpec.describe "GET family_dash/confirmed_jobs" do
   it "contains exactly the keys confirmed, date, end_time, family_id, " +
     "is_assigned, job_id, notes, sitter_id, sitter_name, start_time" do
 
-    get "/family_dash/confirmed_jobs"
+    get "/family_dash/pending_jobs"
 
     expect(json_body[0].keys).to contain_exactly("confirmed", "date", "end_time",
       "family_id", "is_assigned", "job_id", "notes", "sitter_id", "sitter_name",
       "start_time")
   end
 
-  it 'only returns is_assigned: true jobs' do
+  it 'only returns confirmed: false jobs' do
 
-    get "/family_dash/confirmed_jobs"
+    get "/family_dash/pending_jobs"
 
-    expect(json_body[0]["is_assigned"]).to eq true
-  end
-
-  it 'only returns confirmed: true jobs' do
-
-    get "/family_dash/confirmed_jobs"
-
-    expect(json_body[0]["confirmed"]).to eq true
+    expect(json_body[0]["confirmed"]).to eq false
   end
 
   it 'only returns jobs for logged in family' do
-    get '/family_dash/confirmed_jobs'
+    get '/family_dash/pending_jobs'
 
     expect(json_body[0]["family_id"]).to eq @family.id
   end
