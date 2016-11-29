2a0c027f668d4ef14bf577f57242ee8a3990ad12
Lori Burleson <lori.burleson@gmail.com>
2016-11-29 15:11:43 -0500

    updated seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ApplicationRecord
   VALID_ZIP_REGEX = /\d{5}/
   VALID_BIRTHDAY_REGEX = /\d{2}[\/]\d{2}[\/]\d{4}/
   VALID_RATE_REGEX = /\A\d+([.]\d{0,2})?\z/
-  # VALID_PHONE_REGEX = /\(*\+*[1-9]{0,3}\)*-*[1-9]{0,3}[-. \/]*\(*[2-9]\d{2}\)*[-. \/]*\d{3}[-. \/]*\d{4} *e*x*t*\.* *\d{0,4}/
+  VALID_PHONE_REGEX = /\(*\+*[1-9]{0,3}\)*-*[1-9]{0,3}[-. \/]*\(*[2-9]\d{2}\)*[-. \/]*\d{3}[-. \/]*\d{4} *e*x*t*\.* *\d{0,4}/
 
   validates_presence_of :first_name, :last_name, :email, :street, :city, :county
 
@@ -30,7 +30,7 @@ class User < ApplicationRecord
   validates :zip_code, presence: true, format: { with: VALID_ZIP_REGEX }
   validates :birthday, allow_nil: true, format: { with: VALID_BIRTHDAY_REGEX }
   validates :hourly_rate, allow_nil: true, format: { with: VALID_RATE_REGEX }
-  # validates :phone_number, presence: true, format: { with: VALID_PHONE_REGEX }
+  validates :phone_number, presence: true, format: { with: VALID_PHONE_REGEX }
   validate :picture_size
 
   def self.get_active_sitters
